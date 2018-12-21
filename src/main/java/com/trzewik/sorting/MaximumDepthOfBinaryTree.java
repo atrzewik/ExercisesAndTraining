2bbd7ee529d9f4b2c8b01e90a17ae7bceca51b64
atrzewik <42387362+atrzewik@users.noreply.github.com>
2018-12-21 02:04:16 +0100

    BinaryTree train https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/

---
+++ src/main/java/com/trzewik/sorting/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,35 @@
+package com.trzewik.sorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MaximumDepthOfBinaryTree {
+
+        private List<Integer> paths = new ArrayList();
+        private int nodes = 0;
+
+        public int maxDepth(TreeNode root) {
+            findAllPaths(root, nodes);
+            Collections.sort(paths);
+            Collections.reverse(paths);
+            return paths.get(0);
+        }
+
+        private void findAllPaths(TreeNode root, int path){
+            if(root == null){
+                paths.add(path);
+                return;
+            }
+            path += 1;
+            findAllPaths(root.right, path);
+            findAllPaths(root.left, path);
+        }
+
+    class TreeNode {
+        private int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
