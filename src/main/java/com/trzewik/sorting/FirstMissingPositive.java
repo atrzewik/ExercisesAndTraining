cb65012a5198e8b3681b9e31d48969ccc91e672b
atrzewik <42387362+atrzewik@users.noreply.github.com>
2019-01-17 00:38:21 +0100

    Algorithmic exercises https://leetcode.com/problems/first-missing-positive/ https://leetcode.com/problems/trapping-rain-water/

---
+++ src/main/java/com/trzewik/sorting/FirstMissingPositive.java
@@ -0,0 +1,35 @@
+package com.trzewik.sorting;
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+        int currentPositive = 0;
+        if (nums.length > 0) {
+            int[] sortedNums = sortArray(nums);
+            for (int num : sortedNums) {
+                if (num >= 1) {
+                    if (currentPositive + 1 != num && currentPositive != num) {
+                        return currentPositive + 1;
+                    } else if (currentPositive + 1 == num) {
+                        currentPositive++;
+                    }
+                }
+            }
+        }
+        return currentPositive + 1;
+    }
+
+    private int[] sortArray(int[] nums) {
+        for (int j = 0; j < nums.length; j++) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                int currentInteger = nums[i];
+                int nextInteger = nums[i + 1];
+                if (nextInteger < currentInteger) {
+                    nums[i] = nextInteger;
+                    nums[i + 1] = currentInteger;
+                }
+            }
+        }
+        return nums;
+    }
+}
---
+++ src/main/java/com/trzewik/sorting/TrapingRainWater.java
@@ -0,0 +1,36 @@
+package com.trzewik.sorting;
+
+public class TrapingRainWater {
+
+    public int trap(int[] height) {
+        if (height.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        int biggestHeightBefore = height[0];
+        for (int i = 1; i < height.length - 1; i++) {
+            int currentHeight = height[i];
+            int biggestHeightAfter = getBiggestHeightAfter(i, height);
+            if (biggestHeightBefore < currentHeight) {
+                biggestHeightBefore = currentHeight;
+            }
+            if (biggestHeightBefore != currentHeight && biggestHeightBefore <= biggestHeightAfter) {
+                count += biggestHeightBefore - currentHeight;
+            } else if (biggestHeightAfter != currentHeight && biggestHeightAfter < biggestHeightBefore) {
+                count += biggestHeightAfter - currentHeight;
+            }
+        }
+        return count;
+    }
+
+    private int getBiggestHeightAfter(int currentHeightIndex, int[] height) {
+        int biggestHeightAfter = height[currentHeightIndex];
+        for (int i = currentHeightIndex + 1; i < height.length; i++) {
+            int checkingHeightAfter = height[i];
+            if (checkingHeightAfter > biggestHeightAfter) {
+                biggestHeightAfter = checkingHeightAfter;
+            }
+        }
+        return biggestHeightAfter;
+    }
+}
